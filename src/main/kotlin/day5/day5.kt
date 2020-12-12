aa2b0949ecd1b3f9a8ad73e52ac0e1857c40cfbb
PAN <pan910421@gmail.com>
2020-12-12 20:39:30 +0800

    day5

--- src/main/kotlin/day5/day5.kt
+++ src/main/kotlin/day5/day5.kt
@@ -1,6 +1,36 @@
 package day5
 
-fun main() {
+import java.io.File
 
+fun binary(str: String, size: Int): Int {
+    var l = 0
+    var r = size
+    for (s in str) {
+        val middle = (l + r) / 2
+        when (s) {
+            'F', 'L' -> {
+                r = middle
+            }
+            'B', 'R' -> {
+                l = middle
+            }
+        }
+    }
+    return r
+}
+
+fun main() {
+    val input = File("src/main/kotlin/day5/input.txt").readLines()
+    val set = mutableMapOf<Int, Int>()
+    var id: Int
+    var max = 0
+    for (i in input) {
+        id = binary(i.take(7), 127) * 8 + binary(i.takeLast(3), 7)
+        set[id] = id
+        if (max < id) max = id
+    }
+    for (i in set.toSortedMap().firstKey()..set.toSortedMap().lastKey()) {
+        if (!set.containsKey(i)) println(i)
+    }
 }
 
