62b17e8ef711ce49b62dd32dc38c3ef54f632406
PAN <pan910421@gmail.com>
2020-12-22 14:08:46 +0800

    day10

---
+++ src/main/kotlin/day10/day10.kt
@@ -0,0 +1,34 @@
+package day10
+
+
+import java.io.File
+
+//fun part1(file: List<Int>) {
+//    val array = IntArray(2)
+//    for (i in 1..file.lastIndex) {
+//        if (file[i] - file[i - 1] == 1) array[0]++ else array[1]++
+//    }
+//
+//    println((array[0]) * (array[1]))
+//}
+//
+//fun part2(file: MutableList<Int>) {
+//    val list = mutableListOf<Int>()
+//    loop@ for (i in 1..file.lastIndex - 1) {
+//        if (file[i + 1] - file[i - 1] > 3) continue@loop else list.add(file[i])
+//    }
+//    println(list)
+//}
+
+
+fun main() {
+    val input = File("src/main/kotlin/day10/input.txt").readLines().map { it.toInt() }.sorted().toMutableList()
+    input.mapIndexed { index, i -> i - input.getOrElse(index - 1) { 0 } }.plus(3)
+        .let { it.count { i -> i == 1 } * it.count { i -> i == 3 } }.let { println(it) }
+
+    input.fold(mapOf(0 to 1L),
+        { acc, i -> acc.plus(i to (i - 3 until i).map { acc[it] ?: 0 }.sum()) }).let { println(it) }
+
+
+
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,14 @@
 fun main() {
 
+    val numbers = listOf(5, 2, 10, 4)
 
+    val sum = numbers.reduce { sum, element -> sum + element } * 2
+    println(sum)
+    val sumDoubled = numbers.fold(6) { sum, element -> sum + element * 2 }
+    println(sumDoubled)
+
+    val sumDoubledReduce =
+        numbers.reduce { sum, element -> sum + element * 2 } //incorrect: the first element isn't doubled in the result
+    println(sumDoubledReduce)
 }
 
